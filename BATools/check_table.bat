71d8a523d3bb0477ca12c484825b32c7e3d47cb7
lienfeng <13146741545@163.com>
2023-11-14 18:34:01 +0800

    new file

---
+++ BATools/check_table.bat
@@ -0,0 +1,5 @@
+python E:\PyTools\dragon\excel\NFA\check_ActivityTime.py
+python E:\PyTools\dragon\excel\NFA\check_consumeEnergyReward.py
+python E:\PyTools\dragon\excel\NFA\check_partner_exp.py
+python E:\PyTools\dragon\excel\NFA\check_partner.py
+set /p a=enter to close!
